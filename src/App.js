17516bd40cbf2030dc0c52c07df82e9fb3ca9062
Jiramet <gozji0801@gmail.com>
2023-07-17 17:59:28 +0700

    Edit Tasks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-import Main from './components/Main';
+import Tasks from './components/Tasks';
+/* import Users from './components/Users'; */
+/* import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' */
 
 function App() {
   return (
     <main>
-      <Main/>
+      {/* <Router>
+        <Routes>
+          <Route path='/' element={<Users/>}/>
+          <Route path='/task' element={<Tasks/>}/>
+        </Routes>
+      </Router> */}
+      <Tasks/>
     </main>
   );
 }
